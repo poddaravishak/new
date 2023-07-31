2bafc20d95f8ab7dff46223b1132184c21fc4508
avishak-poddar <avishakpoddar@gmail.com>
2023-07-31 17:44:00 +0600

    profile

--- stdprofile.php
+++ stdprofile.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['email']) || !isset($_SESSION['password'])) {
     exit;
 }
 
-// Establish database connection
+// Establish database connection for user information retrieval
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -41,8 +41,7 @@ if (mysqli_num_rows($result) == 1) {
     $coverphoto = $row['coverphoto'];
     $livein = $row['livein'];
     $currently_worked = $row['currently_worked'];
-}
-else {
+} else {
     // Redirect to login page if user information is not found
     header('Location: login.php');
     exit;
@@ -53,183 +52,243 @@ mysqli_close($connection);
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <title>Facebook</title>
-
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
     <link rel="stylesheet" type="text/css" href="css/std.min.css" />
-
     <link rel="stylesheet" type="text/css" href="css/std.css" />
-     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=FontName&display=swap"
-    />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=FontName&display=swap" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 
-  </head>
-
-  <body>
+<body>
     <div class="popop-background"></div>
     <div class="thim-div">
-      <div class="hadr-thim-bar">
-        <span id="thim-button" class="fas fa-caret-right"></span>
-
-        <p>Backgroun</p>
-        <div class="bg-color"></div>
-        <br />
-        <p>Text Color</p>
-        <div class="bg-color"></div>
-      </div>
+        <div class="hadr-thim-bar">
+            <span id="thim-button" class="fas fa-caret-right"></span>
+            <p>Backgroun</p>
+            <div class="bg-color"></div>
+            <br />
+            <p>Text Color</p>
+            <div class="bg-color"></div>
+        </div>
     </div>
 
     <section class="cover-image-section">
-      <header class="cover-hader-site">
-        <img src="<?php echo $coverphoto; ?>" />
-
-        <div class="cover-image-div">
-          <div class="cover-image-edite-btn">
-            <button>
-              <i class="fas fa-camera"></i>
-              Edit Covar Photo
-            </button>
-          </div>
-        </div>
-      </header>
+        <header class="cover-hader-site">
+            <img src="<?php echo $coverphoto; ?>" />
+            <div class="cover-image-div">
+                <div class="cover-image-edite-btn">
+                    <button>
+                        <i class="fas fa-camera"></i>
+                        Edit Covar Photo
+                    </button>
+                </div>
+            </div>
+        </header>
     </section>
 
     <section class="profile-section">
-      <div class="profile-section-in">
-        <div class="profile-image-site">
-          <div class="profile-image-div">
-            <a href="#" id="profile-link">
-              <img id="Profile_images" src="<?php echo $photo; ?>" />
-            </a>
-            <span class="fas fa-camera"></span>
-          </div>
+        <div class="profile-section-in">
+            <div class="profile-image-site">
+                <div class="profile-image-div">
+                    <a href="#" id="profile-link">
+                        <img id="Profile_images" src="<?php echo $photo; ?>" />
+                    </a>
+                    <span class="fas fa-camera"></span>
+                </div>
+            </div>
+            <div class="profile-name-info">
+                <h1>
+                    <span class="pro-txt" id="profile_name"><?php echo $first_name . ' ' . $last_name; ?></span>
+                    <span id="nik-name"></span>
+                </h1>
+                <p></p>
+            </div>
+            <div class="profile-button-site">
+                <div class="btn-site-pro">
+                    <span>
+                        <i class="fas fa-plus-circle"></i>
+                        <a href="edit_profile.php">Edit Profile</a>
+                    </span>
+                </div>
+            </div>
         </div>
-        <div class="profile-name-info">
-          <h1>
-            <span class="pro-txt" id="profile_name"><?php echo $first_name . ' ' . $last_name; ?></span>
-            <span id="nik-name"></span>
-          </h1>
-          <p></p>
-        </div>
-        <div class="profile-button-site">
-          <div class="btn-site-pro">
-            <span>
-              <i class="fas fa-plus-circle"></i>
-              <a href="edit_profile.php">Edit Profile</a>
-            </span>
-            
-          </div>
-        </div>
-      </div>
     </section>
- <!-- The popup form -->
 
     <section class="post-section">
-      <div class="post-section-in">
-        <section class="info-section">
-          <div class="about-info">
-            <h4>Intro</h4>
+        <div class="post-section-in">
+            <section class="info-section">
+                <div class="about-info">
+                    <h4>Intro</h4>
+                    <ul>
+                        <li>
+                            <i class="fas fa-briefcase"></i> Works at
+                            <a href="#"><?php echo $currently_worked; ?></a>
+                        </li>
+                        <li>
+                            <i class="fas fa-id-badge"></i> Batch
+                            <a href="#"><?php echo $batch; ?></a>
+                        </li>
+                        <li>
+                            <i class="fas fa-graduation-cap"></i> Department
+                            <a href="#"><?php echo $department; ?></a>
+                        </li>
+                        <li>
+                            <i class="fas fa-home"></i> Lives in
+                            <a href="#"><?php echo $livein; ?></a>
+                        </li>
+                        <li>
+                            <i class="fa fa-address-book"></i> WhatsApp
+                            <a href="#">https://wa.me/<?php echo $mobile; ?> </a>
+                        </li>
+                    </ul>
+                </div>
+            </section>
 
-            <ul>
-              <li>
-                <i class="fas fa-briefcase"></i> Works at
-                <a href="#"><?php echo $currently_worked; ?></a>
-              </li>
+            <section class="post-info">
+                <div class="box-design">
+                    <div class="post-upload-T">
+                        <div class="profil-ing-div">
+                            <a href="#" id="profile-link">
+                                <img id="Profile_images" src="<?php echo $photo; ?>" />
+                            </a>
+                        </div>
+                        <?php
+                        // Check if the form is submitted
+                        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+                            // Assuming you have a database connection established
+                            $db_host = "localhost";
+                            $db_user = "root";
+                            $db_password = "";
+                            $db_name = "alumni";
 
-              <li>
-                <i class="fas fa-id-badge"></i> Batch
-                <a href="#"><?php echo $batch; ?></a>
-              </li>
-              <li>
-                <i class="fas fa-graduation-cap"></i> depertment
-                <a href="#"><?php echo $department; ?></a>
-              </li>
-              <li>
-                <i class="fas fa-home"></i> Lives in
-                <a href="#"><?php echo $livein; ?></a>
-              </li>
+                            $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
-              <li>
-                <i class="fa fa-address-book"></i> WhatsApp
-                <a href="#"><?php echo $mobile; ?> </a>
-              </li>
-            </ul>
-          </div>
-        </section>
+                            // Check for database connection errors
+                            if ($conn->connect_error) {
+                                die("Connection failed: " . $conn->connect_error);
+                            }
 
-        <section class="post-info">
-          <div class="box-design">
-            <div class="post-upload-T">
-              <div class="profil-ing-div">
-                <a href="#" id="profile-link">
-                  <img id="Profile_images" src="<?php echo $photo; ?>" />
-                </a>
-              </div>
-              <div class="text-post">
-                
-              </div>
-               
-            </div>
-            <div class="photo-upload">
-              <div class="post-upl">
-                <p><i class="fas fa-video"></i> Live Video</p>
-              </div>
-              <div class="post-upl">
-                <p><i class="fas fa-images"></i> Photo/Video</p>
-              </div>
-              <div class="post-upl">
-                <p><i class="fas fa-flag"></i> Life Event</p>
-              </div>
-            </div>
-          </div>
+                            // Retrieve the form data
+                            $post_content = $_POST['post_content'];
+                            $first_name = $_POST['first_name'];
+                            $post_date = $_POST['post_date'];
 
-          <!-- ************ Post ************ -->
+                            // Check if a photo was uploaded
+                            if (isset($_FILES['post_photo']) && $_FILES['post_photo']['error'] === UPLOAD_ERR_OK) {
+                                // Handle the uploaded photo (move it to the "postphoto" folder)
+                                $tmp_name = $_FILES['post_photo']['tmp_name'];
+                                $photo_name = $_FILES['post_photo']['name'];
+                                $photo_path = "postphoto/" . $photo_name; // Save the file path in the database
 
-          <div class="box-design post-div">
+                                move_uploaded_file($tmp_name, $photo_path);
+                            } else {
+                                // No photo was uploaded, set the path to NULL or an appropriate default value
+                                $photo_path = NULL;
+                            }
+
+                            // Insert the data into the "all_post" table
+                            $sql = "INSERT INTO all_post (post_content, student_id, post_date, post_photo) VALUES ('$post_content', '$first_name', '$post_date', '$photo_path')";
+
+                            // Execute the SQL query
+                            if ($conn->query($sql) === TRUE) {
+                                // Post inserted successfully
+                                echo "Post submitted successfully!";
+                            } else {
+                                // Error occurred while inserting the post
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                            }
+
+                            // Close the database connection
+                            $conn->close();
+                        }
+                        ?>
+
+<div class="postdiv" sty>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+        <!-- Textarea for the post content -->
+        <textarea name="post_content" placeholder="Write your post..."></textarea>
+        <?php
+        // Assuming $student_id is set in your PHP code
+        echo '<input type="text" id="first_name" name="first_name" value="' . $student_id . '" style="display: none;">';
+        ?>
+
+        <!-- Date input for auto-selecting today's date -->
+        <input type="date" name="post_date" id="post_date" value="<?php echo date('Y-m-d'); ?>" />
+        <!-- Photo upload option -->
+        <input type="file" name="post_photo" accept="image/*" />
+
+        <!-- Submit button -->
+        <input type="submit" value="Post" />
+    </form>
+</div>
+<section>
+
+<div class="hello" style="display: inline;">
+<?php
+// Establish a new database connection for posts retrieval
+$post_connection = mysqli_connect($servername, $username, $password, $database);
+
+if (!$post_connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Retrieve posts from the all_post table for the current user
+$post_query = "SELECT * FROM all_post WHERE student_id = '$student_id'";
+$post_result = mysqli_query($post_connection, $post_query);
+
+// Check if there are any posts for the current user
+if (mysqli_num_rows($post_result) > 0) {
+    // Loop through each post and display its content
+    while ($post_row = mysqli_fetch_assoc($post_result)) {
+        $post_content = $post_row['post_content'];
+        echo <<<HTML
+        <div class="box-design post-div">
             <div class="post-infarmation">
-              <div>
-                <div class="profil-ing-div post-profile-img">
-                  <a href="#" id="profile-link">
-                    <!-- post user photo -->
-                    <img id="Profile_images" src="<?php echo $photo; ?>" />
-                  </a>
+                <div>
+                    <div class="profil-ing-div post-profile-img">
+                        <a href="#" id="profile-link">
+                            <!-- post user photo -->
+                            <img id="Profile_images" src="$photo" />
+                        </a>
+                    </div>
                 </div>
-              </div>
-              <div class="post-three-dot">
-                <h2><a href="#" id="profile_name"><?php echo $first_name; ?> </a></h2>
-                <p>
-                  <a href="%">date </a>
-                </p>
-
-                <span class="thre-dto-btn fas fa-ellipsis-h"></span>
-              </div>
+                <div class="post-three-dot">
+                    <h2><a href="#" id="profile_name">$first_name</a></h2>
+                    <p><a href="#">date</a></p>
+                    <span class="thre-dto-btn fas fa-ellipsis-h"></span>
+                </div>
             </div>
-
-            <p class="post-text-show">
-              জাভাস্ক্রিপ্ট কি?
-            </p>
-
+            <input type="text" id="first_name" name="first_name" value="$student_id" style="display: none;">
+            <p class="post-text-show">$post_content</p>
             <div class="comment-site">
-              <div class="profil-ing-div">
-                <a href="#" id="profile-link">
-                  <img id="Profile_images" src="images/friends/00.jpg" />
-                </a>
-              </div>
-              <div class="comment-input">
-                <input type="text" placeholder="Write a comment…" />
-                
-                </div>
-              </div>
+                <!-- <div class="profil-ing-div">
+                    <a href="#" id="profile-link">
+                        <img id="Profile_images" src="images/friends/00.jpg" />
+                    </a>
+                </div> -->
+                <!-- <div class="comment-input">
+                    <input type="text" placeholder="Write a comment…" />
+                </div> -->
             </div>
-          </div>
-        </section>
-      </div>
-    </section>
-   
-  </body>
+        </div>
+HTML;
+    }
+} else {
+    // Display a message if there are no posts for the current user
+    echo '<p class="post-text-show">No posts found.</p>';
+}
+
+// Close the connection for posts retrieval
+mysqli_close($post_connection);
+?>
+</div>
+</section>
+    <script>
+        // JavaScript to set the date input to the current date
+        document.getElementById("post_date").valueAsDate = new Date();
+    </script>
+</body>
 </html>
